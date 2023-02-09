7915e7124f2042a807af873c2d695c85b200d221
Daria Sannikova <sannikovade@gmail.com>
2023-02-09 22:45:57 +0300

    Second task

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,11 @@ public class Main {
         people.add(new Person<>("Joe", "van Smuth", 60));
         people.add(new Person("Sacha", "Baron Cohen", 51));
         people.add(new Person("Arthur", "Elliot-Murray-Kynynmound", 25));
-        people.add(new Person<>("John", "Smith", 55));
+        people.add(new Person<>("Jane", "Smith", 55));
+        people.add(new Person("Alice", "van Albert-Lynn-Alice", 33));
+        people.add(new Person("Andrew", "Persival junior the third", 40));
         System.out.println(people);
-        Collections.sort(people, new WhoIsMoreNoble());
+        Collections.sort(people, new WhoIsMoreNoble(4).reversed());
         System.out.println(people);
     }
 }
