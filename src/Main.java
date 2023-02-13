80837235a135a6c18993302ea719ce27e0ccb902
Daria Sannikova <sannikovade@gmail.com>
2023-02-13 22:16:14 +0300

    Eraser added

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,20 @@
 import java.util.*;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
         List<Person> people = new ArrayList<>();
         people.add(new Person<>("Joe", "van Smuth", 60));
-        people.add(new Person("Sacha", "Baron Cohen", 51));
+        people.add(new Person("Sacha", "Baron Cohen", 8));
         people.add(new Person("Arthur", "Elliot-Murray-Kynynmound", 25));
         people.add(new Person<>("Jane", "Smith", 55));
-        people.add(new Person("Alice", "van Albert-Lynn-Alice", 33));
+        people.add(new Person("Alice", "van Albert-Lynn-Alice", 12));
         people.add(new Person("Andrew", "Persival junior the third", 40));
         System.out.println(people);
         Collections.sort(people, new WhoIsMoreNoble(4).reversed());
+        System.out.println(people);
+        Predicate<Person> isYoung = person -> person.getAge() < 18;
+        people.removeIf(isYoung);
         System.out.println(people);
     }
 }
