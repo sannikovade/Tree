44cdaffdc16a8930b560ea753f73168c50cd821e
Daria Sannikova <sannikovade@gmail.com>
2023-02-13 21:57:40 +0300

    Lambda added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.util.*;
 
+
+
 public class Main {
     public static void main(String[] args) {
         List<Person> people = new ArrayList<>();
@@ -10,7 +12,22 @@ public class Main {
         people.add(new Person("Alice", "van Albert-Lynn-Alice", 33));
         people.add(new Person("Andrew", "Persival junior the third", 40));
         System.out.println(people);
-        Collections.sort(people, new WhoIsMoreNoble(4).reversed());
+
+        Comparator<Person> whoIsMoreNoble = (o1,o2) -> {
+            int surname1 = o1.getSurname().split("\\P{IsAlphabetic}+").length;
+            int surname2 = o2.getSurname().split("\\P{IsAlphabetic}+").length;
+            if (surname1 >= 4 && surname2 >= 4) {
+                return Integer.compare(o1.getAge(), o2.getAge());
+            }
+            if (surname1 != surname2) {
+                return Integer.compare(surname1, surname2);
+            } else {
+                return Integer.compare(o1.getAge(), o2.getAge());
+            }
+        };
+
+
+        Collections.sort(people, whoIsMoreNoble.reversed());
         System.out.println(people);
     }
 }
